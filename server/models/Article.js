21966b25c62ecea469012f729c3af6266814a985
yuelin.ye <yuelin.ye@xbongbong.com>
2021-08-10 19:11:40 +0800

    feat: init model

---
+++ server/models/Article.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: yuelin.ye
+ * @Date: 2021-08-10 17:51:16
+ * @LastEditors: yuelin.ye
+ * @LastEditTime: 2021-08-10 17:51:16
+ * @Description: 
+ */
---
+++ server/models/User.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: yuelin.ye
+ * @Date: 2021-08-10 17:51:32
+ * @LastEditors: yuelin.ye
+ * @LastEditTime: 2021-08-10 17:51:33
+ * @Description: 
+ */
